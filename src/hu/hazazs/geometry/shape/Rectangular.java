605d184a0c165ee1c0751700dae643b02ade9352
Tibor Hazafi <hazafi.tibor@gmail.com>
2023-01-03 21:13:45 +0100

    Add rectangular shape

--- src/hu/hazazs/geometry/shape/Rectangular.java
+++ src/hu/hazazs/geometry/shape/Rectangular.java
@@ -1,5 +1,33 @@
 package hu.hazazs.geometry.shape;
 
-final class Rectangular extends Shape {
+import hu.hazazs.geometry.InputHandler;
+import hu.hazazs.geometry.metric.MetricUnit;
+
+public final class Rectangular extends Shape {
+
+	private Side A;
+	private Side B;
+
+	@Override
+	public void getAndSetSidesAndAngles(InputHandler handler) {
+		A = handler.getSide("A");
+		B = handler.getSide("B");
+	}
+
+	@Override
+	public void printAreaAndPerimeter(MetricUnit unit) {
+		double convertedA = convert(A, unit);
+		double convertedB = convert(B, unit);
+		printAreaAndPerimeter(calculateArea(convertedA, convertedB), calculatePerimeter(convertedA, convertedB),
+				unit.getUnit());
+	}
+
+	private double calculateArea(double convertedA, double convertedB) {
+		return convertedA * convertedB;
+	}
+
+	private double calculatePerimeter(double convertedA, double convertedB) {
+		return 2 * (convertedA + convertedB);
+	}
 
 }
